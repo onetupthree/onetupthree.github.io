21aa212d3b6bd9149c2d1cc88728ceeb8c4b16e3
Tup <ramon.luisking14@gmail.com>
2020-08-07 08:36:44 +0800

    initial push

---
+++ precache-manifest.b979b1d52abb65741707385894613bb3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7573bba654745d9e4cac933a6a900721",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91356173daa7b88989f7",
+    "url": "/static/css/main.9b56a0ea.chunk.css"
+  },
+  {
+    "revision": "5b706a12afea8d7d0ce9",
+    "url": "/static/js/2.dcbfe179.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.dcbfe179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91356173daa7b88989f7",
+    "url": "/static/js/main.f8bc5672.chunk.js"
+  },
+  {
+    "revision": "65e6f54ab0e1a1056c5b",
+    "url": "/static/js/runtime-main.740691d1.js"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "5e1d162e634a102d7da8db597825a764",
+    "url": "/static/media/SourceSansPro-Regular.5e1d162e.otf"
+  },
+  {
+    "revision": "84e363e610534abdf6c5d0daa4c344f6",
+    "url": "/static/media/gyparody.84e363e6.ttf"
+  },
+  {
+    "revision": "19156af93053612a8cb1b72f2e736111",
+    "url": "/static/media/leaguegothic-regular-webfont.19156af9.ttf"
+  }
+]);
